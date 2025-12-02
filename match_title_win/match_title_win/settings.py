c520891f5f66db66790ffe8fc3b1a08d97d2bfe2
richurichu <paulsonrichu@gmail.com>
2025-12-02 17:31:30 +0530

    new commit

--- match_title_win/match_title_win/settings.py
+++ match_title_win/match_title_win/settings.py
@@ -34,7 +34,12 @@ load_dotenv()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [ '127.0.0.1',
+    'localhost',
+    'localhost:5173/',
+    'match-tile-win-game.vercel.app',
+    'web-production-34e07.up.railway.app/'
+]
 
 
 CSRF_TRUSTED_ORIGINS = [
